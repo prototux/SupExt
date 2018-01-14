6f395b565289ab40c920922d60821c2646ecae1e
prototux <jason@prototux.net>
2018-01-14 17:21:07 +0100

    Move paramiko sshclient creation in run() to avoid race conditions when multiple threads does ssh checks, should fix #6

--- supext/modules/tests/ssh.py
+++ supext/modules/tests/ssh.py
@@ -2,10 +2,6 @@ import time
 import paramiko
 
 class ssh:
-    def __init__(self):
-        self.ssh = paramiko.SSHClient()
-        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
     def run(self, config):
         ret = { 'ok': None, 'message': None }
 
@@ -15,10 +11,18 @@ class ssh:
             ret['message'] = 'No server to check, or missing username'
             return ret
 
+        try:
+            ssh_client = paramiko.SSHClient()
+            ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        except:
+            ret['ok'] = False
+            ret['message'] = 'SSH Client error'
+            return ret
+
         # Try to connect to SSH
         try:
-            self.ssh.connect(config['host'], port=config.get('port', 22), username=config['user'], password=config.get('password'), key_filename=config.get('keyfile'))
-            self.ssh.close()
+            ssh_client.connect(config['host'], port=config.get('port', 22), username=config['user'], password=config.get('password'), key_filename=config.get('keyfile'))
+            ssh_client.close()
         except:
             ret['ok'] = False
             ret['message'] = 'Cannot connect to server'
